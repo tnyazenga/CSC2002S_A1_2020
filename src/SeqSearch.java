f1e81de725d0abcded26a5392fe6a9918ecc1e7e
Tafadzwa Nyazenga <nyztaf001@myuct.ac.za>
2020-08-27 01:01:15 +0200

    updated seqsearch

---
+++ src/SeqSearch.java
@@ -0,0 +1,102 @@
+import java.util.*;
+import java.io.*;
+
+public class SeqSearch {
+    public static void main (String[] args) throws Exception {
+        Scanner systemScan = new Scanner(System.in);
+        System.out.println("Input file "+ args[0]);
+        File file;
+        file = new File("","./Data/"+args[0]);
+        Scanner fileScan = new Scanner(file);
+        float[][] testArray = readArray(file);
+        //Terrain Test = new Terrain(testArray);
+        //boolean wudUp = Test.isBasin(154, 212);
+        //System.out.println(wudUp);
+        double time1 = System.currentTimeMillis();
+        for (int i = 0; i < testArray.length; i ++) {
+            for (int j = 0; j < testArray[0].length; j ++){
+                if (isBasin(i, j, testArray) == true) {
+                    System.out.print(i + "  " + j);
+                    System.out.println();
+                }
+            }
+        }
+        double time2 =System.currentTimeMillis() - time1;
+        System.out.println(time2);
+    }
+    public static float[][] readArray(File file) throws FileNotFoundException {
+        Scanner scan = new Scanner(file);
+        String info = scan.nextLine();
+        System.out.println(info);
+        String[] infoSplit = info.split(" ");
+        int dim1 = Integer.parseInt(infoSplit[0]);
+        int dim2 = Integer.parseInt(infoSplit[1]);
+        float[][] array = new float[dim1][dim2];
+        while (scan.hasNextLine()){
+            String[] line = scan.nextLine().trim().split(" ");
+            int i = 0;
+            int count = 0;
+            while (i < dim1) {
+                for (int j = 0; j < dim2 ; j ++){
+                    array[i][j] = Float.parseFloat(line[count]);
+                    count ++;
+                }
+                i ++;
+            }
+        }
+        return array;
+    }
+
+    public static boolean isBasin(int rowNum, int columnNum, float[][] mountainArray){
+        if (rowNum == 0 && columnNum == 0){    // Top left
+            //if (mountainArray[0][1] - mountainArray[0][0] >= 0.01 && mountainArray[1][1] - mountainArray[0][0] >= 0.01 &&
+            //      mountainArray[1][0] - mountainArray[0][0] >= 0.01)
+            return false;
+        }
+        else if (rowNum == 0 && columnNum == mountainArray.length -1 ) { // Top Right
+            //if (mountainArray[0][mountainArray.length - 2] - mountainArray[0][mountainArray.length - 1] >= 0.01 && mountainArray[1][mountainArray.length - 2] - mountainArray[0][mountainArray.length - 1] >= 0.01 &&
+            //      mountainArray[1][mountainArray.length-1] - mountainArray[0][mountainArray.length - 1] >= 0.01)
+            return false;
+        }
+        else if(rowNum == mountainArray[0].length -1  && columnNum == 0){ // Bottom left
+            //if (mountainArray[mountainArray[0].length - 2][0] - mountainArray[mountainArray[0].length-1][0] >= 0.01 && mountainArray[mountainArray[0].length - 2][1] - mountainArray[mountainArray[0].length-1][0] >= 0.01 &&
+            //      mountainArray[mountainArray[0].length -1][1] - mountainArray[mountainArray[0].length - 1][0] >= 0.01)
+            //return true;
+        }
+        else if(rowNum == mountainArray[0].length - 1 && columnNum == mountainArray.length - 1){   // Bottom right
+            //if (mountainArray[mountainArray[0].length - 2][mountainArray.length - 1] - mountainArray[mountainArray[0].length-1][mountainArray.length - 1] >= 0.01 &&
+            //      mountainArray[mountainArray[0].length - 2][mountainArray.length - 2] - mountainArray[mountainArray[0].length-1][mountainArray.length - 1] >= 0.01 &&
+            //    mountainArray[mountainArray[0].length -1][mountainArray.length -2] - mountainArray[mountainArray[0].length - 1][mountainArray.length - 1] >= 0.01)
+            return false;
+        }
+        else if (columnNum == 0){   // Left of box
+            //if (mountainArray[rowNum - 1][columnNum] - mountainArray[rowNum][columnNum] >= 0.01 && mountainArray[rowNum-1][columnNum + 1] - mountainArray[rowNum][columnNum] >= 0 &&
+            //      mountainArray[rowNum][columnNum + 1] - mountainArray[rowNum][columnNum] >= 0 &&
+            //    mountainArray[rowNum + 1][columnNum] - mountainArray[rowNum][columnNum] >= 0 && mountainArray[rowNum + 1][columnNum + 1] - mountainArray[rowNum][columnNum] >= 0.01)
+            return false;
+        }
+        else if (columnNum == mountainArray.length - 1) {   // Right of box
+            //if (mountainArray[rowNum - 1][columnNum] - mountainArray[rowNum][columnNum] >= 0.01 && mountainArray[rowNum - 1][columnNum -1] - mountainArray[rowNum][columnNum] >= 0.01 &&
+            //      mountainArray[rowNum][columnNum -1] - mountainArray[rowNum][columnNum] >= 0 &&
+            //    mountainArray[rowNum + 1][columnNum] - mountainArray[rowNum][columnNum] >= 0 && mountainArray[rowNum + 1][columnNum-1] - mountainArray[rowNum][columnNum] >= 0)
+            return false;
+        }
+        else if (rowNum == 0){   // Top of box
+            //if (mountainArray[rowNum][columnNum + 1] - mountainArray[rowNum][columnNum] >= 0 && mountainArray[rowNum][columnNum -1] - mountainArray[rowNum][columnNum] >= 0 &&
+            //      mountainArray[rowNum + 1][columnNum] - mountainArray[rowNum][columnNum] >= 0 && mountainArray[rowNum + 1][columnNum + 1] - mountainArray[rowNum][columnNum] >= 0.01 && mountainArray[rowNum - 1][columnNum-1] - mountainArray[rowNum][columnNum] >= 0)
+            return false;
+        }
+        else if (rowNum == mountainArray[0].length - 1) { //Bottom of box
+            //if (mountainArray[rowNum - 1][columnNum] - mountainArray[rowNum][columnNum] >= 0.01 && mountainArray[rowNum-1][columnNum + 1] - mountainArray[rowNum][columnNum] >= 0 && mountainArray[rowNum + 1][columnNum -1] - mountainArray[rowNum][columnNum] >= 0.01 &&
+            //      mountainArray[rowNum][columnNum + 1] - mountainArray[rowNum][columnNum] >= 0 && mountainArray[rowNum][columnNum -1] - mountainArray[rowNum][columnNum] >= 0 )
+            return false;
+        }
+        else {  // General case
+            if (mountainArray[rowNum + 1][columnNum] - mountainArray[rowNum][columnNum] >= 0.01 && mountainArray[rowNum+1][columnNum + 1] - mountainArray[rowNum][columnNum] >= 0.01 && mountainArray[rowNum + 1][columnNum -1] - mountainArray[rowNum][columnNum] >= 0.01 &&
+                    mountainArray[rowNum][columnNum + 1] - mountainArray[rowNum][columnNum] >= 0.01 && mountainArray[rowNum][columnNum -1] - mountainArray[rowNum][columnNum] >= 0.01 &&
+                    mountainArray[rowNum - 1][columnNum] - mountainArray[rowNum][columnNum] >= 0.01 && mountainArray[rowNum - 1][columnNum + 1] - mountainArray[rowNum][columnNum] >= 0.01 && mountainArray[rowNum - 1][columnNum-1] - mountainArray[rowNum][columnNum] >= 0.01)
+                return true;
+        }
+        return false;
+    }
+}
